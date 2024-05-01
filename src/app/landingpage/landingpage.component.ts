243fa01e6e6b8f6910d5a189cccf060a2ee5e798
Illutas <107239220+Illutas@users.noreply.github.com>
2024-05-01 09:55:59 +0200

    Update height of .atf class in above-the-fold.component.scss Add app-about-me component to landingpage.component.html Import AboutMeComponent in landingpage.component.ts Update subproject commit in shared directory

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from '../above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [AboveTheFoldComponent],
+  imports: [AboveTheFoldComponent, AboutMeComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
