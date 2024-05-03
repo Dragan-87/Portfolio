504237f06ce4f0aae3db59683e144872a0163fd3
Illutas <107239220+Illutas@users.noreply.github.com>
2024-05-03 18:27:10 +0200

    Update landing page with Portfolio component

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from '../above-the-fold/above-the-fold.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { MySkillsComponent } from '../my-skills/my-skills.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [AboveTheFoldComponent, AboutMeComponent, MySkillsComponent],
+  imports: [
+    AboveTheFoldComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    PortfolioComponent
+  ],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
