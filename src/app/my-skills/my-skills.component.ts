3cd974c004d84605f64978854c4a844258a2dc36
Illutas <107239220+Illutas@users.noreply.github.com>
2024-05-03 13:39:18 +0200

    Update positioning and image paths in above-the-fold component

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
+  techIcons = [
+    {
+      
+    }
+  ]
 
 }
