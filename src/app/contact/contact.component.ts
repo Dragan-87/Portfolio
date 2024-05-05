b1894d72f3f497df5af8825efb94ae4ee118f38e
Illutas <107239220+Illutas@users.noreply.github.com>
2024-05-05 22:51:47 +0200

    Update contact form with required fields and button functionality

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,6 @@
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -8,21 +8,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    FormsModule
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-  name: string = '';
-  email: string = '';
-  message: string = '';
+
+  contactObj: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]*$')]),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(50), Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')]),
+    message: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(5000), Validators.pattern('^[a-zA-Z0-9.,!? ]*$')]),
+    privacyPolicy: new FormControl(false, [Validators.requiredTrue])
+  });
+
+  messageForm!: FormGroup;
 
   constructor() {
+
   }
 
-
   submitData() {
-
   }
 }
