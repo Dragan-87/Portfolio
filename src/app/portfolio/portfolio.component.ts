94a9b10d11ede1beec4c8d72793059571e8369da
Illutas <107239220+Illutas@users.noreply.github.com>
2024-05-17 16:54:53 +0200

    chore: Update component styles, responsive stylesheets, and mobile view styles

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ProjectCardComponent } from './project-card/project-card.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [ProjectCardComponent, ReactiveFormsModule],
+  imports: [ProjectCardComponent, FormsModule],
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss', './portfolio-responsive.scss']
 })
@@ -19,7 +19,7 @@ export class PortfolioComponent {
       techStack: 'JavaScript | HTML | CSS',
       projectDescription: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
       githubLink: 'https://github.com/Dragan-87/join',
-      liveLink: ''
+      liveLink: 'http://join.dragan-saric.de/'
     },
     {
       projectImg: 'el-pollo-loco',
@@ -27,7 +27,7 @@ export class PortfolioComponent {
       techStack: 'JavaScript | HTML | CSS',
       projectDescription: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and salsa bottles to fight against the killer chicken.',
       githubLink: 'https://github.com/Dragan-87/el-pollo-loco',
-      liveLink: 'https://dragan-saric.de/el-pollo-loco/index.html'
+      liveLink: 'http://el-pollo-loco.dragan-saric.de/',
     },
 
   ];
