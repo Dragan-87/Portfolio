df6b07120fdf4c3995b4273d310d7da12d6ce884
Illutas <107239220+Illutas@users.noreply.github.com>
2024-05-18 21:51:44 +0200

    chore: Update app.routes.ts to import Router from @angular/router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,10 @@
-import { Routes, provideRouter } from '@angular/router';
+import { provideRouter, Router } from '@angular/router';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-
-export const routes: Routes = [
+export const routes = [
   { path: '', component: LandingpageComponent },
   { path: 'imprint', component: ImprintComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent }
-
-];
-
-export const APP_ROUTER_PROVIDERS = [
-  provideRouter(routes)
 ];
